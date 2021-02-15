597d1aba7ba6f9b0d96dd7731affc3694c599233
ChatchonS <chatchonnick@gmail.com>
2021-02-15 20:12:53 +0700

    commit

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,6 +1,16 @@
+import { Button } from 'antd';
 import React from 'react';
+import LocalStorageService from '../../services/localStorageService';
+import {Link} from 'react-router-dom';
 
-export default function Profile() {
+export default function Profile(props) {
+
+    const logout = () => {
+        LocalStorageService.removeToken();
+        props.setRole("guest");
+    };
+
+
     return (
         <div>
             <h2>
@@ -11,6 +21,8 @@ export default function Profile() {
                 <br />
                 <strong>User ID:</strong> 15
             </p>
+            <Link to="/todo"><Button>Goto todo</Button></Link> 
+            <Button onClick= {logout}>Logout</Button>
         </div>
     );
 }
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,42 @@
+import { notification } from 'antd';
 import axios from 'axios';
+import localStorageService from '../services/localStorageService';
+import LocalStorageService from '../services/localStorageService';
 
 axios.defaults.baseURL = "http://localhost:8000";
+
+axios.interceptors.request.use(
+    config => {
+      if(config.url.includes("/login") || config.url.includes("/register")) return config;
+      
+      const token = LocalStorageService.getToken();
+
+      if(token){
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+    },
+    err => {
+        Promise.reject(err);
+    }
+)
+
+axios.interceptors.response.use(
+  response => {
+    return response
+  },
+  err => {
+    if(err.response?.state === 401){
+      localStorageService.removeToken();
+      window.location.reload();
+      notification.error({
+        message: "กรุณาเข้าสู่ระบบใหม่"
+      });
+      return Promise.reject(err);
+    }
+    
+    return Promise.reject(err);
+
+  }
+);
 
 export default axios;
